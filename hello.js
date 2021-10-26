89d914e58e1f42dac80f14f9b59c94b901a52b32
amyfranses <amyfranses@gmail.com>
2021-10-26 19:40:56 +0000

    packing-list js practice

--- hello.js
+++ hello.js
@@ -2,3 +2,14 @@ const sayHello = function(name) {
   console.log("Hello, " + (name));
 }
 sayHello();
+
+const sayHelloToConsole = function(name){
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John');
+
+const returnSayHello = function(name) {
+  return "hello, " + name;
+}
+const greeting = returnSayHello("John");
+console.log(greeting);
---
+++ loopyLighthouse.js
@@ -0,0 +1,19 @@
+var i = 100;
+while (i < 200){
+  if (i % 3 === 0) {
+    console.log("Loopy");
+  } else if (i % 4 === 0){
+    console.log("Lighthouse");
+  } else if ((i % 3 === 0) && (i % 4 === 0)){
+    console.log("LoopyLighthouse");
+    }  else {
+    console.log(i);
+  }
+    
+i++;
+}
+
+
+
+  
+
